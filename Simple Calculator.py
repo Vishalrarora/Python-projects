fdcbab640a49debb7dd7efb96bf4292ef5a0f554
Vishalrarora <158090638+Vishalrarora@users.noreply.github.com>
2024-01-30 18:27:25 +0530

    Update Simple Calculator.py

--- Simple Calculator.py
+++ Simple Calculator.py
@@ -1,5 +1,3 @@
-Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
 >>> def calculator(a, b, x):
 ...         if x == '+':
 ...              return(str(a) + ' ' + x + ' ' + str(b) + ' = ' + str(a + b))
@@ -22,8 +20,3 @@ Type "help", "copyright", "credits" or "license()" for more information.
 ... 
 ...     
 >>> main()
-Please type the first number: 8
-please type the second number: 9
-What operation would you like to carry out? 
-Choose between "+,-,*,/": -
-8 - 9 = -1
