4eb47aaa08e79f9208a668f6f0d663fead8e6671
Vishalrarora <158090638+Vishalrarora@users.noreply.github.com>
2024-01-30 19:56:19 +0530

    Update 6 Digit Password Generator.py

--- 6 Digit Password Generator.py
+++ 6 Digit Password Generator.py
@@ -1,5 +1,3 @@
-Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
 >>> import random
 >>> letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o','p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 >>> numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -16,4 +14,3 @@ Type "help", "copyright", "credits" or "license()" for more information.
 ... 
 ...     
 >>> main()
-A9#83t
